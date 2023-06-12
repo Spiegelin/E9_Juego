75316dceb6e46676a0de01131a3a11a804ae3d08
Spiegelin <diego15.espejo@gmail.com>
2023-06-12 12:47:09 -0600

    PersonajeGuerrero hpp

---
+++ Proyecto_Equipo9/assignments/Proyecto/PersonajeGuerrero.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Personaje.hpp"
+
+class PersonajeGuerrero: public Personaje {
+    public:
+        // Constructores
+        PersonajeGuerrero();
+        PersonajeGuerrero(std::string, std::string, Mundo*, int, int, int);
+
+        // Getters
+        int getSaludTotal();
+        int getSaludActual();
+        int getMaxAtaque();
+
+        // Setters
+        void setSaludTotal(int);
+        void setSaludActual(int);
+        void getMaxAtaque(int);
+
+        // Métodos
+        bool isAlive();
+        int porcentajeSalud();
+        void barraVida();
+        void imprime() override;
+        int juega() override;
+        void recibeInteraccion(int) override;
+    private:
+        int saludTotal;
+        int saludActual;
+        int maxAtaque;
+};
